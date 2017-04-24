d0245cc9fe35f4757a58df5b0428d9f52c7bc1be
Tanja Stroble <plusdeltah@gmail.com>
2017-04-24 13:54:17 -0700

    Ex5
    I was bored so I didn't follow instructions.

---
+++ ex5.rb
@@ -0,0 +1,22 @@
+my_name = "Tanja Stroble"
+my_age = 32
+my_height_in_inches = 68
+my_height_in_cm = my_height_in_inches * 2.54
+my_weight = 155
+my_bmi = 23.6
+
+if my_bmi > 25
+  health = "overweight"
+else
+  health = "not overweight"
+end
+
+if health == "not overweight"
+    conclusion = "Hooray for me!"
+elsif health == "overweight"
+  conclusion = "And I will only eat lettuce until I sort myself out."
+end
+
+puts "My name is #{my_name}.  I am #{my_age}. I am #{my_height_in_inches} inches
+or #{my_height_in_cm}cm tall. Accodring to my bmi of #{my_bmi}, I am #{health}."
+puts conclusion
