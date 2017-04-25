554803f2b7b1cb0cbaa33ef21b8df57a5c4e141d
Tanja Stroble <plusdeltah@gmail.com>
2017-04-25 12:55:43 -0700

    Ex5

--- ex5.rb
+++ ex5.rb
@@ -3,7 +3,8 @@ my_age = 32
 my_height_in_inches = 68
 my_height_in_cm = my_height_in_inches * 2.54
 my_weight = 155
-my_bmi = 23.6
+puts "Enter your bmi: "
+my_bmi = gets.chomp
 
 if my_bmi > 25
   health = "overweight"
@@ -12,7 +13,7 @@ else
 end
 
 if health == "not overweight"
-  conclusion = "Hooray for me!"
+    conclusion = "Hooray for me!"
 elsif health == "overweight"
   conclusion = "And I will only eat lettuce until I sort myself out."
 end
